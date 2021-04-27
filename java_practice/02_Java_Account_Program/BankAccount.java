3c8230dbb880d128b1c6f6ff70e4814b96fe13f4
olive-su <1466su@naver.com>
2021-04-27 22:40:12 +0900

    setter, getter method adding

--- java_practice/02_Java_Account_Program/BankAccount.java
+++ java_practice/02_Java_Account_Program/BankAccount.java
@@ -8,14 +8,14 @@ public class BankAccount {
     // 리턴 : 성공여부(불린)
     boolean deposit(int amount) {
         // 1. write code here
-        if ((amount >= 0) && (owner.cashAmount >= amount)){
+        if ((amount >= 0) && (owner.getCashAmount() >= amount)){
             balance += amount;
-            owner.cashAmount -= amount;
-            System.out.println(amount + "원 입금하였습니다. "+"잔고: "+balance+"원, 현금: "+owner.cashAmount+"원");
+            owner.setCashAmount(owner.getCashAmount() - amount);
+            System.out.println(amount + "원 입금하였습니다. "+"잔고: "+balance+"원, 현금: "+owner.getCashAmount()+"원");
             return true;
         }
         else {
-            System.out.println("입금 실패입니다. 잔고: "+balance+"원, 현금: "+owner.cashAmount+"원");
+            System.out.println("입금 실패입니다. 잔고: "+balance+"원, 현금: "+owner.getCashAmount()+"원");
             return false;
         }
     }
@@ -26,12 +26,12 @@ public class BankAccount {
         // 2. write code here
         if (amount >= 0 && balance >= amount){
             balance -= amount;
-            owner.cashAmount += amount;
-            System.out.println(amount + "원 출금하였습니다. "+"잔고: "+balance+"원, 현금: "+owner.cashAmount+"원");
+            owner.setCashAmount(owner.getCashAmount() + amount);
+            System.out.println(amount + "원 출금하였습니다. "+"잔고: "+balance+"원, 현금: "+owner.getCashAmount()+"원");
             return true;
         }
         else {
-            System.out.println("출금 실패입니다. 잔고: "+balance+"원, 현금: "+owner.cashAmount+"원");
+            System.out.println("출금 실패입니다. 잔고: "+balance+"원, 현금: "+owner.getCashAmount()+"원");
             return false;
         }
     }
--- java_practice/02_Java_Account_Program/BankDriver.java
+++ java_practice/02_Java_Account_Program/BankDriver.java
@@ -4,21 +4,21 @@ public class BankDriver {
     public static void main(String[] args) {
         // 사람 선언
         Person p1 = new Person();
-        p1.name = "John";
-        p1.age = 28;
-        p1.cashAmount = 30000;
+        p1.setName("John");
+        p1.setAge(28);
+        p1.setCashAmount(30000);
 
         // 은행 계좌 생성
         BankAccount a1 = new BankAccount();
         a1.balance = 100000;
 
-        p1.account = a1;
+        p1.setAccount(a1);
         a1.owner = p1;
 
         Person p2 = new Person();
-        p2.name = "Demon";
-        p2.age = 25;
-        p2.cashAmount = 100000;
+        p2.setName ("Demon");
+        p2.setAge(25);
+        p2.setCashAmount(100000);
 
         BankAccount a2 = new BankAccount();
         a2.balance = 500000;
